5ea0c86bdfeef1e62ed0935369ad95d7246a6e72
LuckyLuckyLu <alu8@stuy.edu>
2017-11-28 16:40:51 -0500

    Created abstract LibraryBook class

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,27 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+
+    public LibraryBook(String writer, String name, String ID, String extraNumber){
+	super(writer,name,ID);
+	callNumber = extraNumber;
+	
+    }
+    public String getCallNumber(){
+	return callNumber;
+    }
+    public void setCallNumber(String extraNumber){
+	callNumber = extraNumber;
+    }
+    public int compareTo(LibraryBook other){
+	if (getCallNumber.compareTo(other.getCallNumber)<0) { return -1}
+   	if (getCallNumber.compareTo(other.getCallNumber)>0) { return 1}
+	else {return 0}
+    }
+    public abstract void checkout(String patron, String due);
+
+    public abstract void returned();
+
+    public String circulationStatus();
+    
+
+}
