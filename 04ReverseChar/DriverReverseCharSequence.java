24f1f130208eae42b7555efc45ddc4026304abed
LuckyLuckyLu <alu8@stuy.edu>
2018-01-07 14:34:28 -0500

    Renamed java file to proper file name. Replaced getData method with toString() method.

--- 04ReverseChar/DriverReverseCharSequence.java
+++ 04ReverseChar/DriverReverseCharSequence.java
@@ -1,13 +1,18 @@
 public class DriverReverseCharSequence{
   public static void main(String[] args){
-    ReverseCharSequence x = new ReverseCharSequence();
-    System.out.println(x.getData());
-    ReverseCharSequence y = new ReverseCharSequence("SequenceOfCharacters");
-    System.out.println(x.getData());
+    ReversedCharSequence x = new ReversedCharSequence();
+    System.out.println(x);
+    ReversedCharSequence y = new ReversedCharSequence("SequenceOfCharacters");
+    System.out.println(x);
     System.out.println(x.charAt(0));
     System.out.println(x.subSequence(0,3));
-    System.out.println(y.getData());
+    System.out.println(y);
     System.out.println(y.charAt(0));
     System.out.println(y.subSequence(0,3));
+    ReversedCharSequence a = new ReversedCharSequence("hello");
+    System.out.println(a);
+    System.out.println(a.length());
+    System.out.println(a.charAt(3));
+    System.out.println(a.subSequence(0,3));
   }
 }
